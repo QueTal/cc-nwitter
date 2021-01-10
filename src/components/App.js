adefc821f9c246fabb14d0338b3636d3096fb889
quetal <gibum1995@gmail.com>
2021-01-10 23:49:38 +0900

    auth error

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router"; 
+import {authService}  from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (<>
+  <AppRouter isLoggedIn={isLoggedIn}/> 
+  <footer>&copy; Nwitter{new Date().getFullYear()} </footer>
+  </>
+  ); 
 }
 
 export default App;
